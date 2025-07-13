1d187f2dd2dcc8103fa10cd4e5f743930f69bc12
Jefferson Santos <jeffersonsantos_7@hotmail.com>
2025-07-13 02:54:08 -0300

    Sétimo commit

--- Controlai.Web/program.cs
+++ Controlai.Web/program.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.Extensions.Configuration;
 
+using Applicacao.Servicos;
+using Dominio.Interfaces;
+using Infraestrutura.Repositorio;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ConnectString.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IRepoUsuarioSistema, RepoUsuarioSistema>();
+builder.Services.AddScoped<SvcObterUsuario, 
+
 
 var app = builder.Build();
 
