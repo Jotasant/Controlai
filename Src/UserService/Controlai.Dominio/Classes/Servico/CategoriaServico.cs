2cdc7593db9d43707db00d921f83dc2cef90084c
Jefferson Santos <jeffersonsantos_7@hotmai.com>
2025-08-17 23:37:07 -0300

    Commit Linux

--- Src/UserService/Controlai.Dominio/Classes/Servico/CategoriaServico.cs
+++
@@ -1,8 +0,0 @@
-namespace Dominio.Models;
-
-public class CategoriaServico
-{
-    public int id { get; set; } // ID da categoria
-    public string? Nome { get; set; } // Nome da categoria
-    public string? Descricao { get; set; } // Descrição da categoria
-}
